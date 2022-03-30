c733ae19276d0d53ddee656fc900cce3d3ccc792
Peter May <peter.may@winzana.com>
2022-03-30 09:53:55 +0200

    feat(person): add method to add new person

--- src/person/actions/index.ts
+++ src/person/actions/index.ts
@@ -3,4 +3,5 @@ export * from './get-one.action';
 export * from './get-position.action';
 export * from './get-persons.actions';
 export * from './get-config.action';
+export * from './post-person-new.actions';
 export * from './post-person-register.actions';
