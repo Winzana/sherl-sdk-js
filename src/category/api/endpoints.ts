8d5d1f2180dcc4b0c1d50295e6a16269b5f7b71c
Peter May <peter.may@winzana.com>
2022-04-05 16:35:10 +0200

    refactor(category): remove comment lines

--- src/category/api/endpoints.ts
+++ src/category/api/endpoints.ts
@@ -2,7 +2,7 @@ export const endpoints = {
   POST_CATEGORY_NEW: '/api/shop/products/categories',
   POST_NEW_SUB_CATEGORY: '/api/shop/products/categories/:id',
   GET__CATEGORIS: '/api/shop/products/categories?organizationId=:id',
-  GET_SUB_CATEGORIES: '/api/shop/products/categories/:id', //not complete
+  GET_SUB_CATEGORIES: '/api/shop/products/categories/:id',
   GET_MAIN_AND_SUB_CATEGORIES:
     '/api/shop/products/categories/all?organizationId=:id',
   GET_PUBLIC_CATEGORIES: '/api/shop/products/categories/public',
