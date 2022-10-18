109eb6abb7525e945ba14595c9045e833546386a
David Chmarzynski <david@sherl.io>
2022-10-18 14:51:44 +0200

    refactor(api): regroup product | order | discount in new shop domain

--- src/product/actions/get-categories.action.ts
+++
@@ -1,9 +0,0 @@
-import { ICategoryResponse } from '../types';
-import { ProductApi } from '../api/client';
-
-export const getCategories = async (
-  organizationId: string,
-): Promise<ICategoryResponse[]> => {
-  const response = await ProductApi.getCategories(organizationId);
-  return response.data;
-};
