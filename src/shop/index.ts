78e38f6903a75bac1d2290e1eedb2e87b0b6825f
David Chmarzynski <david@sherl.io>
2022-10-18 14:08:00 +0200

    refactor(api): fix shop domain name

--- src/shop/index.ts
+++ src/shop/index.ts
@@ -2,4 +2,4 @@ import * as discount from './actions/discount';
 import * as product from './actions/product';
 import * as order from './actions/order';
 
-export const actions = { ...discount, ...product, ...order };
+export const shop = { ...discount, ...product, ...order };
