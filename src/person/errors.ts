12cd768048c190b0c9bc7091e3f6c47e542a5d85
Anaïs <52666656+Asaysay@users.noreply.github.com>
2023-01-24 13:02:11 +0100

    feat(person): add route to update person by id, SI-159 (#97)
    Co-authored-by: anaiswinzana <anais@winzana.com>

--- src/person/errors.ts
+++ src/person/errors.ts
@@ -1,10 +1,17 @@
 import { ErrorFactory } from '../common/errors';
 
-type Err = 'fetch-failed' | 'not-found';
+export enum PersonErr {
+  PUT_FAILED = 'put-person-failed',
+}
 
 export const errors = {
   'fetch-failed': 'Failed to fetch person API',
   'not-found': 'Person not found',
+  [PersonErr.PUT_FAILED]: 'Failed to update person',
 };
 
-export const errorFactory = new ErrorFactory<Err>('person', 'Person', errors);
+export const errorFactory = new ErrorFactory<PersonErr>(
+  'person',
+  'Person',
+  errors,
+);
