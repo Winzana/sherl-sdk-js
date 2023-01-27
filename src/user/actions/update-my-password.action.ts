d7124a3d86d8b6907f4e217e3e23185e4c2b7749
anaiswinzana <anais@winzana.com>
2023-01-27 11:24:00 +0100

    feat(SI-202): changed updateMyPassword return type

--- src/user/actions/update-my-password.action.ts
+++ src/user/actions/update-my-password.action.ts
@@ -1,27 +1,20 @@
-import { ApiResponse, Fetcher } from '../../common/api';
+import { Fetcher } from '../../common/api';
 import { endpoints } from '../api/endpoints';
-import { IUpdatePasswordDto } from '../types';
 import { errorFactory, UserErr } from '../errors';
+import { IUpdatePasswordDto } from '../types';
 
 export const updateMyPassword = async (
   fetcher: Fetcher,
   data: Partial<IUpdatePasswordDto>,
-): Promise<IUpdatePasswordDto> => {
-  let response: ApiResponse<IUpdatePasswordDto> | null = null;
-
+): Promise<boolean> => {
   try {
-    response = await fetcher
+    await fetcher
       .post<IUpdatePasswordDto>(endpoints.UPDATE_MY_PASSWORD, data)
       .catch(() => {
         throw errorFactory.create(UserErr.UPDATE_MY_PASSWORD_FAILED);
       });
+    return true;
   } catch (err) {
     throw new Error('Cannot reach API');
   }
-
-  if (!response.data) {
-    throw errorFactory.create(UserErr.UPDATE_MY_PASSWORD_FAILED);
-  }
-
-  return response.data;
 };
