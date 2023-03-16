535052d45c5f4808d36e40f7fe5f9a10984c2b06
benoitguelin <benoit.guelin@winzana.com>
2023-03-16 14:39:41 +0100

    feat(organization): import correction

--- src/person/index.ts
+++ src/person/index.ts
@@ -1,6 +1,6 @@
 import { SherlClient } from '../common';
 import { PersonProvider } from './provider';
-import { IPerson, IPersonFilters } from './types';
+import { IPerson, IPersonFilters, IOpeningHoursSpecification, IImageObject } from './types';
 
 const person = (client: SherlClient) => new PersonProvider(client);
-export { person, IPerson, IPersonFilters };
+export { person, IPerson, IPersonFilters, IOpeningHoursSpecification, IImageObject };
