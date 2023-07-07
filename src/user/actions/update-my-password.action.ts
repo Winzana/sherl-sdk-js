2da2d31cac6aae1c3a41685ae629a8cb50667737
ff.winzana <florian.fournier@winzana.com>
2023-07-07 12:15:57 +0200

    feat(SI-502): remove unnecessary try/catch

--- src/user/actions/update-my-password.action.ts
+++ src/user/actions/update-my-password.action.ts
@@ -7,15 +7,10 @@ export const updateMyPassword = async (
   fetcher: Fetcher,
   data: Partial<IUpdatePasswordDto>,
 ): Promise<boolean> => {
-  try {
-    await fetcher
-      .post<IUpdatePasswordDto>(endpoints.UPDATE_MY_PASSWORD, data)
-      .catch(() => {
-        throw errorFactory.create(UserErr.UPDATE_MY_PASSWORD_FAILED);
-      });
-    return true;
-  } catch (err) {
-    console.log(err);
-    return false;
-  }
+  await fetcher
+    .post<IUpdatePasswordDto>(endpoints.UPDATE_MY_PASSWORD, data)
+    .catch(() => {
+      throw errorFactory.create(UserErr.UPDATE_MY_PASSWORD_FAILED);
+    });
+  return true;
 };
