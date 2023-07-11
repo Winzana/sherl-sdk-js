65e2ed2dfb74f1b1d1d3bf713191c03799e81c22
FlorianWinzana <111342385+FlorianWinzana@users.noreply.github.com>
2023-07-11 11:31:26 +0200

    feat(user): add reset password route (#129)

--- src/user/api/endpoints.ts
+++ src/user/api/endpoints.ts
@@ -1,3 +1,4 @@
 export const endpoints = {
   UPDATE_MY_PASSWORD: '/api/user/password/change',
+  RESET_PASSWORD_VALIDATE: '/api/user/password/forgot-validate',
 };
--- src/user/errors.ts
+++ src/user/errors.ts
@@ -4,6 +4,7 @@ export enum UserErr {
   PUT_FAILED = 'user/put-user-failed',
   POST_FAILED = 'user/post-user-failed',
   UPDATE_MY_PASSWORD_FAILED = 'user/update-password-failed',
+  RESET_PASSWORD_VALIDATE_FAILED = 'user/reset-password-failed',
   FETCH_FAILED = 'user/fetch-failed',
   NOT_FOUND = 'user/not-found',
 }
@@ -14,6 +15,7 @@ export const errors = {
   [UserErr.PUT_FAILED]: 'Failed to update user',
   [UserErr.POST_FAILED]: 'Failed to post user',
   [UserErr.UPDATE_MY_PASSWORD_FAILED]: 'Failed to update password',
+  [UserErr.RESET_PASSWORD_VALIDATE_FAILED]: 'Failed to reset password',
 };
 
 export const errorFactory = new ErrorFactory<UserErr>('User', errors);
