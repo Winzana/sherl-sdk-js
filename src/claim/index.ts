a077ef5875a704e7ac1adde8010c77094bf7fa29
winzana-mg <75690727+winzana-mg@users.noreply.github.com>
2023-09-20 17:15:41 +0200

    docs(claim): fix with missing interfaces (#141)

--- src/claim/index.ts
+++ src/claim/index.ts
@@ -5,7 +5,19 @@ import {
   IClaim,
   IClaimCreate,
   IClaimUpdate,
+  ClaimStatusEnum,
+  IClaimReply,
+  ISchedules,
 } from './types';
 
 const claim = (client: SherlClient) => new ClaimProvider(client);
-export { claim, IClaimTicketFilters, IClaim, IClaimCreate, IClaimUpdate };
+export {
+  claim,
+  IClaimTicketFilters,
+  IClaim,
+  IClaimCreate,
+  IClaimUpdate,
+  ClaimStatusEnum,
+  IClaimReply,
+  ISchedules,
+};
