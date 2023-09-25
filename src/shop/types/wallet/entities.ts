206c553b4073d8ec60076ba0c656f84c7f004160
winzana-mg <75690727+winzana-mg@users.noreply.github.com>
2023-09-25 11:00:35 +0200

    docs(organization): add wallet type docs and refactor rib action types (#144)

--- src/shop/types/wallet/entities.ts
+++ src/shop/types/wallet/entities.ts
@@ -2,3 +2,14 @@ export interface IWallet {
   userId: string;
   walletId: string;
 }
+
+export interface IAddRibBody {
+  iban: string;
+  bic: string;
+}
+
+export interface IRib {
+  iban: string;
+  bic: string;
+  ibanId?: string;
+}
