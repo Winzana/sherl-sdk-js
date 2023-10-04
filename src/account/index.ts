6fe44d914ee129a7991dcd07492338642224c061
David Chmarzynski <david.chm00@gmail.com>
2023-10-04 09:44:35 +0200

    feat(SHER-663): fix build & sidebars file

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,6 +1,6 @@
 import { SherlClient } from '../common';
 import { AccountProvider } from './provider';
-import { IAccount, IAccountCreateInputDto, IAddressDto } from './types';
+import { IAccount, IAccountCreateInputDto } from './types';
 
 const account = (client: SherlClient) => new AccountProvider(client);
-export { account, IAccount, IAccountCreateInputDto, IAddressDto };
+export { account, IAccount, IAccountCreateInputDto };
