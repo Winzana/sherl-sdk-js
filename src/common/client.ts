708ff6f580de32e7b2c6290a327ac132c4008631
David Chmarzynski <david.chm00@gmail.com>
2023-11-14 11:58:21 +0100

    feat(SHER-670): fix resetOptions

--- src/common/client.ts
+++ src/common/client.ts
@@ -1,8 +1,16 @@
-import { initializeApi, registerBearerToken } from './api';
+import { initializeApi, registerBearerToken, resetOptions } from './api';
 import { ErrorFactory, CommonErr } from './errors';
-import { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { AxiosInstance } from 'axios';
 
 export interface InitOptions {
+  apiKey: string;
+  apiSecret: string;
+  apiUrl?: string;
+  referer?: string;
+}
+
+export interface resetOptions {
+  axiosInstance: AxiosInstance;
   apiKey: string;
   apiSecret: string;
   apiUrl?: string;
@@ -17,6 +25,16 @@ export class SherlClient {
 
   constructor(private readonly options: InitOptions) {
     this.apiInstance = initializeApi(
+      options.apiKey,
+      options.apiSecret,
+      options.apiUrl,
+      options.referer,
+    );
+  }
+
+  public resetOptions(options: resetOptions) {
+    this.apiInstance = resetOptions(
+      options.axiosInstance,
       options.apiKey,
       options.apiSecret,
       options.apiUrl,
@@ -58,54 +76,3 @@ export function init(options: InitOptions) {
 
   return new SherlClient(options);
 }
-
-/**
- * Axios configuration
- */
-interface CustomAxiosRequestConfig extends Omit<AxiosRequestConfig, 'headers'> {
-  headers?: any; // this was "any" at v0.21.1 but now broken between 0.21.4 >= 0.27.2
-  // Lets make it any again to make it work again.
-}
-
-// RESET ACTUEL AXIOS INSTANCE
-export const resetAxiosInstance = (
-  axiosInstance: AxiosInstance,
-  apiKey: string,
-  apiSecret: string,
-  apiUrl?: string,
-  referer?: string,
-): AxiosInstance => {
-  axiosInstance.defaults.baseURL = apiUrl || 'https://api.sherl.io';
-
-  axiosInstance.defaults.headers.common = {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer',
-  };
-
-  axiosInstance.interceptors.request.eject(0);
-
-  axiosInstance.interceptors.request.use(
-    (config: CustomAxiosRequestConfig) => {
-      if (typeof apiKey === 'undefined' || typeof apiSecret === 'undefined') {
-        throw errorFactory.create(CommonErr.MISSING_CREDENTIALS);
-      }
-
-      config.headers.common['X-WZ-API-KEY'] = apiKey;
-      config.headers.common['X-WZ-API-SECRET'] = apiSecret;
-
-      if (referer) {
-        config.headers.common['Referer'] = referer;
-      }
-
-      if (config.headers.Authorization) {
-        config.headers.Authorization = null;
-      }
-
-      return config;
-    },
-    (error) => Promise.reject(error),
-  );
-
-  return axiosInstance;
-};
