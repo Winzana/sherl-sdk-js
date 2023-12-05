482102eec621495a14272e0b9b58f6756b44bd78
David Chmarzynski <david.chm00@gmail.com>
2023-12-05 16:51:08 +0100

    feat(SHER-710): add organization backgroundImage JSDocs

--- src/organization/actions/BackgroundImage/create-background-image.action.ts
+++ src/organization/actions/BackgroundImage/create-background-image.action.ts
@@ -5,6 +5,25 @@ import { endpoints } from '../../api/endpoints';
 import { OrganizationErr, errorFactory } from '../../errors';
 import { IOrganizationResponse } from '../../types';
 
+/**
+ * Creates a background image for an organization by uploading a file.
+ *
+ * This function sends a POST request to upload and set a background image for an organization using a file object.
+ * The organization's unique ID and media ID are used to construct the endpoint, and the file is sent as form data.
+ * An optional onUploadProgress function can be provided to monitor the upload progress. On successful upload and
+ * creation, it returns the updated organization's information encapsulated in an IOrganizationResponse object.
+ * If the upload or creation process encounters any errors, such as a non-200 status response or connectivity issues,
+ * a specific error indicating the failure of creating the background image is thrown.
+ *
+ * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+ * @param {string} organizationId - The unique identifier of the organization for which the background image is being set.
+ * @param {string} mediaId - The unique identifier of the media associated with the background image.
+ * @param {File} file - The image file to be uploaded and set as the background.
+ * @param {(progressEvent: any) => void} [onUploadProgress] - Optional callback to monitor the progress of the upload.
+ * @returns {Promise<IOrganizationResponse>} A promise that resolves to the updated organization's information after the background image creation.
+ * @throws {OrganizationErr.CREATE_BACKGROUND_IMAGE_FAILED} Throws an error if the background image creation fails.
+ */
+
 export const createBackgroundImage = async (
   fetcher: Fetcher,
   organizationId: string,
