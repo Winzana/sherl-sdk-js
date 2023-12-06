1bc82043f6b4c8f465f6f947235f5a91e94ed39b
David Chmarzynski <david.chm00@gmail.com>
2023-12-06 16:33:25 +0100

    feat(SHER-710): add organization OpenHoursSpecification JSDocs

--- src/organization/actions/OpeningHoursSpecification/create-opening-hours-specification.action.ts
+++ src/organization/actions/OpeningHoursSpecification/create-opening-hours-specification.action.ts
@@ -7,6 +7,22 @@ import {
   IOrganizationResponse,
 } from '../../types';
 
+/**
+ * Creates an opening hours specification for a specified organization.
+ *
+ * This function sends a POST request to set the opening hours for an organization. It requires the organization's
+ * unique ID to construct the endpoint and the opening hours details provided in the IOpeningHoursSpecificationInputDto.
+ * On successful creation, it returns the updated organization's information encapsulated in an IOrganizationResponse object.
+ * If the creation process encounters any errors, such as a non-201 status response or connectivity issues, a specific error
+ * indicating the failure of creating the opening hours specification is thrown.
+ *
+ * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+ * @param {string} organizationId - The unique identifier of the organization for which the opening hours are being set.
+ * @param {IOpeningHoursSpecificationInputDto} data - The details of the opening hours specification to be created.
+ * @returns {Promise<IOrganizationResponse>} A promise that resolves to the updated organization's information after creating the opening hours specification.
+ * @throws {OrganizationErr.CREATE_OPENING_HOURS_SPECIFICATION_FAILED} Throws an error if the creation of the opening hours specification fails.
+ */
+
 export const createOpeningHoursSpecification = async (
   fetcher: Fetcher,
   organizationId: string,
--- src/organization/actions/OpeningHoursSpecification/delete-opening-hours-specification.action.ts
+++ src/organization/actions/OpeningHoursSpecification/delete-opening-hours-specification.action.ts
@@ -4,6 +4,22 @@ import { endpoints } from '../../api/endpoints';
 import { OrganizationErr, errorFactory } from '../../errors';
 import { IOrganizationResponse } from '../../types';
 
+/**
+ * Deletes an opening hours specification from a specified organization.
+ *
+ * This function sends a DELETE request to remove an opening hours specification from an organization. It requires the
+ * organization's unique ID and the opening hours specification's unique ID to construct the endpoint for the request.
+ * On successful deletion, it returns the updated organization's information encapsulated in an IOrganizationResponse object.
+ * If the deletion process encounters any errors, such as a non-200 status response or connectivity issues, a specific error
+ * indicating the failure of deleting the opening hours specification is thrown.
+ *
+ * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+ * @param {string} organizationId - The unique identifier of the organization from which the opening hours specification is being deleted.
+ * @param {string} hoursSpecId - The unique identifier of the opening hours specification to be deleted.
+ * @returns {Promise<IOrganizationResponse>} A promise that resolves to the updated organization's information after the deletion of the opening hours specification.
+ * @throws {OrganizationErr.DELETE_OPENING_HOURS_SPECIFICATION_FAILED} Throws an error if the deletion of the opening hours specification fails.
+ */
+
 export const deleteOpeningHoursSpecification = async (
   fetcher: Fetcher,
   organizationId: string,
