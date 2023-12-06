548a30535dadd12ded399f4137b2e228942a7f7f
David Chmarzynski <david.chm00@gmail.com>
2023-12-06 16:08:02 +0100

    feat(SHER-710): add organization employee JSDocs

--- src/organization/actions/Employee/create-employee.action.ts
+++ src/organization/actions/Employee/create-employee.action.ts
@@ -4,6 +4,22 @@ import { endpoints } from '../../api/endpoints';
 import { OrganizationErr, errorFactory } from '../../errors';
 import { IEmployee, IOrganizationMemberInputDto } from '../../types';
 
+/**
+ * Creates a new employee record for a specified organization.
+ *
+ * This function sends a POST request to add a new employee to an organization. It requires the organization's unique ID
+ * to construct the endpoint, and the employee's details are provided in the IOrganizationMemberInputDto object.
+ * On successful creation, it returns the newly created employee's information encapsulated in an IEmployee object.
+ * If the employee creation process encounters any errors, such as a non-201 status response or connectivity issues,
+ * a specific error indicating the failure of the employee creation is thrown.
+ *
+ * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+ * @param {string} organizationId - The unique identifier of the organization to which the employee is being added.
+ * @param {IOrganizationMemberInputDto} employee - The details of the employee to be added.
+ * @returns {Promise<IEmployee>} A promise that resolves to the information of the newly created employee.
+ * @throws {OrganizationErr.CREATE_EMPLOYEE_FAILED} Throws an error if the employee creation fails.
+ */
+
 export const createEmployee = async (
   fetcher: Fetcher,
   organizationId: string,
--- src/organization/actions/Employee/update-employee.action.ts
+++ src/organization/actions/Employee/update-employee.action.ts
@@ -4,6 +4,24 @@ import { endpoints } from '../../api/endpoints';
 import { OrganizationErr, errorFactory } from '../../errors';
 import { IEmployee, IOrganizationMemberInputDto } from '../../types';
 
+/**
+ * Updates an employee's details within a specified organization.
+ *
+ * This function sends a PUT request to modify the details of an existing employee in an organization.
+ * It requires the organization's unique ID and the employee's unique ID to construct the endpoint for the request.
+ * The updated employee details are provided in a Partial<IOrganizationMemberInputDto> object. On successful update,
+ * it returns the information of the updated employee encapsulated in an IEmployee object. If the update process
+ * encounters any errors, such as a non-200 status response or connectivity issues, a specific error indicating
+ * the failure of the employee update is thrown.
+ *
+ * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+ * @param {string} organizationId - The unique identifier of the organization to which the employee belongs.
+ * @param {string} employeeId - The unique identifier of the employee to be updated.
+ * @param {Partial<IOrganizationMemberInputDto>} updatedEmployee - The partial data of the employee to be updated.
+ * @returns {Promise<IEmployee>} A promise that resolves to the information of the updated employee.
+ * @throws {OrganizationErr.UPDATE_EMPLOYEE_FAILED} Throws an error if the employee update fails.
+ */
+
 export const updateEmployee = async (
   fetcher: Fetcher,
   organizationId: string,
