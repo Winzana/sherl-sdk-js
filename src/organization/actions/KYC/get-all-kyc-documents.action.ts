9d07c6cfda05e23deecd8447772648a1bc00cefd
David Chmarzynski <david.chm00@gmail.com>
2023-12-06 16:21:25 +0100

    feat(SHER-710): add organization KYC JSDocs

--- src/organization/actions/KYC/get-all-kyc-documents.action.ts
+++ src/organization/actions/KYC/get-all-kyc-documents.action.ts
@@ -4,6 +4,21 @@ import { endpoints } from '../../api/endpoints';
 import { OrganizationErr, errorFactory } from '../../errors';
 import { IKYCDocument } from '../../types';
 
+/**
+ * Retrieves all KYC documents for a specified organization.
+ *
+ * This function sends a GET request to fetch all KYC documents associated with an organization. It uses the
+ * organization's unique ID to construct the endpoint for the request. On successful retrieval, it returns an array
+ * of KYC documents, each encapsulated in an IKYCDocument object. If the retrieval process encounters any errors,
+ * such as a non-200 status response or connectivity issues, a specific error indicating the failure of fetching the
+ * KYC documents is thrown.
+ *
+ * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+ * @param {string} organizationId - The unique identifier of the organization for which the KYC documents are being retrieved.
+ * @returns {Promise<IKYCDocument[]>} A promise that resolves to an array of KYC documents for the specified organization.
+ * @throws {OrganizationErr.GET_KYCS_FAILED} Throws an error if the retrieval of KYC documents fails.
+ */
+
 export const getAllKycDocuments = async (
   fetcher: Fetcher,
   organizationId: string,
--- src/organization/actions/KYC/update-kyc-document.action.ts
+++ src/organization/actions/KYC/update-kyc-document.action.ts
@@ -5,6 +5,25 @@ import { endpoints } from '../../api/endpoints';
 import { OrganizationErr, errorFactory } from '../../errors';
 import { IKYCDocument } from '../../types';
 
+/**
+ * Updates a specific KYC document for an organization.
+ *
+ * This function sends a PUT request to update a KYC document associated with an organization. It requires the
+ * organization's unique ID and the KYC document's unique ID to construct the endpoint for the request. The updated
+ * document details are provided in the IImageObject. An optional onUploadProgress function can be provided to
+ * monitor the upload progress. On successful update, it returns the updated KYC document's information encapsulated
+ * in an IKYCDocument object. If the update process encounters any errors, such as a non-200 status response or
+ * connectivity issues, a specific error indicating the failure of updating the KYC document is thrown.
+ *
+ * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+ * @param {string} organizationId - The unique identifier of the organization to which the KYC document belongs.
+ * @param {string} kycId - The unique identifier of the KYC document to be updated.
+ * @param {IImageObject} document - The updated KYC document details.
+ * @param {(progressEvent: any) => void} [onUploadProgress] - Optional callback to monitor the progress of the upload.
+ * @returns {Promise<IKYCDocument>} A promise that resolves to the information of the updated KYC document.
+ * @throws {OrganizationErr.UPDATE_DOCUMENT_FAILED} Throws an error if the KYC document update fails.
+ */
+
 export const updateKycDocument = async (
   fetcher: Fetcher,
   organizationId: string,
