53eaf70a0df4906bf2e64c9a144f705fec493e60
johan Affre <johan@winzana.com>
2023-12-12 16:23:44 +0100

    feat(SHER-732): add error place JSDoc

--- src/place/actions/get-places.action.ts
+++ src/place/actions/get-places.action.ts
@@ -2,6 +2,7 @@ import { Fetcher } from '../../common/api';
 import { endpoints } from '../api/endpoints';
 import { IPlace } from '../types';
 import { Pagination } from '../../common';
+import { PlaceErr, errorFactory } from '../errors';
 
 export const getPlaces = async (
   fetcher: Fetcher,
@@ -9,17 +10,29 @@ export const getPlaces = async (
   itemsPerPage = 10,
   filters: { [key: string]: any },
 ): Promise<Pagination<IPlace>> => {
-  const response = await fetcher.get<Pagination<IPlace>>(endpoints.GET_PLACES, {
-    page,
-    itemsPerPage,
-    ...filters,
-  });
-
-  if (response.status !== 200) {
-    throw new Error(
-      `Failed to fetch products API (status: ${response.status})`,
+  try {
+    const response = await fetcher.get<Pagination<IPlace>>(
+      endpoints.GET_PLACES,
+      {
+        page,
+        itemsPerPage,
+        ...filters,
+      },
     );
-  }
 
-  return response.data;
+    switch (response.status) {
+      case 200:
+        return response.data;
+      case 403:
+        throw errorFactory.create(PlaceErr.FETCH_PLACES_FORBIDDEN);
+      case 404:
+        throw errorFactory.create(PlaceErr.FETCH_PLACES_NOT_FOUND);
+      case 409:
+        throw errorFactory.create(PlaceErr.FETCH_PLACES_ALREADY_EXIST);
+      default:
+        throw errorFactory.create(PlaceErr.FETCH_FAILED);
+    }
+  } catch (error) {
+    throw errorFactory.create(PlaceErr.FETCH_FAILED);
+  }
 };
