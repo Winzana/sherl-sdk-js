da2b12ed8ad069a505810be3a9739f8a8979f339
johan Affre <johan@winzana.com>
2023-12-12 16:49:55 +0100

    feat(SHER-733): add error quotas JSDoc

--- src/quotas/actions/get-quota-find-one-by.action.ts
+++ src/quotas/actions/get-quota-find-one-by.action.ts
@@ -14,7 +14,16 @@ export const getQuotaFindOneBy = async (
       filters,
     );
 
-    return response.data;
+    switch (response.status) {
+      case 200:
+        return response.data;
+      case 403:
+        throw errorFactory.create(QuotaErr.FETCH_QUOTA_FIND_ONE_BY_FORBIDDEN);
+      case 404:
+        throw errorFactory.create(QuotaErr.FETCH_QUOTA_FIND_ONE_BY_NOT_FOUND);
+      default:
+        throw errorFactory.create(QuotaErr.FETCH_FAILED);
+    }
   } catch (error) {
     throw errorFactory.create(QuotaErr.FETCH_FAILED);
   }
--- src/quotas/errors/errors.ts
+++ src/quotas/errors/errors.ts
@@ -2,10 +2,18 @@ import { ErrorFactory } from '../../common/errors';
 
 export enum QuotaErr {
   FETCH_FAILED = 'quota/fetch-failed',
+  FETCH_QUOTA_FIND_ONE_BY_FORBIDDEN = 'quota/fetch-find-one-by-forbidden',
+  FETCH_QUOTA_FIND_ONE_BY_NOT_FOUND = 'quota/fetch-find-one-not-found',
+  FETCH_QUOTA_FIND_ONE_BY_ALREADY_EXISTS = 'quota/fetch-find-one-by-already-exists',
 }
 
 export const errors = {
   [QuotaErr.FETCH_FAILED]: 'Failed to fetch quota',
+  [QuotaErr.FETCH_QUOTA_FIND_ONE_BY_FORBIDDEN]: 'Fetch quota failed forbidden',
+  [QuotaErr.FETCH_QUOTA_FIND_ONE_BY_NOT_FOUND]:
+    'Failed to fetch  not found quota',
+  [QuotaErr.FETCH_QUOTA_FIND_ONE_BY_ALREADY_EXISTS]:
+    'Failed to fetch already exists quota',
 };
 
 export const errorFactory = new ErrorFactory<QuotaErr>('Quota', errors);
