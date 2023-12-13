c3f4f63c52abaa9cecbc90afc75b05f59e9de029
David Chmarzynski <david.chm00@gmail.com>
2023-12-13 09:54:27 +0100

    feat(SHER-718): add account error

--- src/account/actions/create-account.action.ts
+++ src/account/actions/create-account.action.ts
@@ -1,4 +1,5 @@
 import { Fetcher } from '../../common/api';
+import { getSherlError } from '../../common/utils';
 import { endpoints } from '../api/endpoints';
 import { AccountErr, errorFactory } from '../errors';
 import { IAccount, IAccountCreateInputDto } from '../types/entities';
@@ -12,11 +13,23 @@ export const createAccount = async (
       endpoints.CREATE_ACCOUNT,
       data,
     );
-    if (response.status !== 201) {
-      throw errorFactory.create(AccountErr.CREATE_ACCOUNT_FAILED);
+    switch (response.status) {
+      case 201:
+        return response.data;
+      case 403:
+        throw errorFactory.create(AccountErr.CREATE_ACCOUNT_FORBIDDEN);
+      case 404:
+        throw errorFactory.create(AccountErr.CREATE_ACCOUNT_NOT_FOUND);
+      case 409:
+        throw errorFactory.create(AccountErr.CREATE_ACCOUNT_ALREADY_EXISTS);
+      default:
+        throw errorFactory.create(AccountErr.CREATE_ACCOUNT_FAILED);
     }
-    return response.data;
   } catch (error) {
-    throw errorFactory.create(AccountErr.CREATE_ACCOUNT_FAILED);
+    const sherlError = getSherlError(
+      error,
+      errorFactory.create(AccountErr.CREATE_ACCOUNT_FAILED),
+    );
+    throw sherlError;
   }
 };
