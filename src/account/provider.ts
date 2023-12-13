4099207f90680a0287a88553a95296f334a07a9c
David Chmarzynski <david.chm00@gmail.com>
2023-12-13 12:00:06 +0100

    feat(SHER-710): add AccountProvider JSDoc

--- src/account/provider.ts
+++ src/account/provider.ts
@@ -7,6 +7,20 @@ class AccountProvider extends AbstractProvider {
   constructor(client: SherlClient) {
     super(client, errorFactory);
   }
+
+  /**
+   * Creates a new account with the provided data.
+   *
+   * This function sends a POST request to create a new account using the data provided in the IAccountCreateInputDto object.
+   * On successful account creation, it returns the newly created account's information. If the creation process encounters
+   * any errors, such as a non-201 status response or connectivity issues, a specific error indicating the failure of the account
+   * creation is thrown.
+   *
+   * @param {Fetcher} fetcher - The fetcher instance used for making API requests.
+   * @param {IAccountCreateInputDto} data - The data for creating a new account.
+   * @returns {Promise<IAccount>} A promise that resolves to the newly created account's information.
+   * @throws {AccountErr.CREATE_ACCOUNT_FAILED} Throws an error if the account creation fails.
+   */
   public createAccount = this.withFetcher(createAccount);
 }
 
