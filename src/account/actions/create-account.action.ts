8a8e52c5fb095cf42e292ad5eb2b9c760eb7b5fd
David Chmarzynski <david.chm00@gmail.com>
2023-12-13 14:44:25 +0100

    feat(SHER-718): fix threads

--- src/account/actions/create-account.action.ts
+++ src/account/actions/create-account.action.ts
@@ -18,18 +18,13 @@ export const createAccount = async (
         return response.data;
       case 403:
         throw errorFactory.create(AccountErr.CREATE_ACCOUNT_FORBIDDEN);
-      case 404:
-        throw errorFactory.create(AccountErr.CREATE_ACCOUNT_NOT_FOUND);
-      case 409:
-        throw errorFactory.create(AccountErr.CREATE_ACCOUNT_ALREADY_EXISTS);
       default:
         throw errorFactory.create(AccountErr.CREATE_ACCOUNT_FAILED);
     }
   } catch (error) {
-    const sherlError = getSherlError(
+    throw getSherlError(
       error,
       errorFactory.create(AccountErr.CREATE_ACCOUNT_FAILED),
     );
-    throw sherlError;
   }
 };
--- src/account/errors.ts
+++ src/account/errors.ts
@@ -3,18 +3,12 @@ import { ErrorFactory } from '../common/errors';
 export enum AccountErr {
   CREATE_ACCOUNT_FAILED = 'account/create-account-failed',
   CREATE_ACCOUNT_FORBIDDEN = 'account/create-account-forbidden',
-  CREATE_ACCOUNT_NOT_FOUND = 'account/create-account-not-found',
-  CREATE_ACCOUNT_ALREADY_EXISTS = 'account/create-account-already-exists',
 }
 
 export const errors = {
   [AccountErr.CREATE_ACCOUNT_FAILED]: 'Failed to create account',
   [AccountErr.CREATE_ACCOUNT_FORBIDDEN]:
     'Failed to create account. Forbidden access',
-  [AccountErr.CREATE_ACCOUNT_NOT_FOUND]:
-    'Failed to create account. Page not found',
-  [AccountErr.CREATE_ACCOUNT_ALREADY_EXISTS]:
-    'Failed to create account. Account already exists',
 };
 
 export const errorFactory = new ErrorFactory<AccountErr>('Account', errors);
