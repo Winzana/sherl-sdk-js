ec627b936fc260254d16a9ae6cddb49d591bd3c2
jg-winzana <102950530+jg-winzana@users.noreply.github.com>
2023-12-18 15:20:55 +0100

    feat(shop-invoice): update errors code

--- src/shop/errors/invoice/errors.ts
+++ src/shop/errors/invoice/errors.ts
@@ -1,11 +1,16 @@
 import { ErrorFactory } from '../../../common/errors';
 
 export enum InvoiceErr {
-  SEND_FAILED = 'loyalty/send-failed',
+  SEND_INVOICE_LINK_FAILED = 'loyalty/send-failed',
+  SEND_INVOICE_LINK_FAILED_FORBIDDEN = 'loyalty/send-failed-forbidden',
+  INVOICE_ID_NOT_FOUND = 'loyalty/invoice-id-not-found',
 }
 
 export const errors = {
-  [InvoiceErr.SEND_FAILED]: 'Failed to send link',
+  [InvoiceErr.SEND_INVOICE_LINK_FAILED]: 'Failed to send link',
+  [InvoiceErr.SEND_INVOICE_LINK_FAILED_FORBIDDEN]:
+    'Failed to send link, forbidden',
+  [InvoiceErr.INVOICE_ID_NOT_FOUND]: 'Invoice not found',
 };
 
 export const errorFactory = new ErrorFactory<InvoiceErr>(
