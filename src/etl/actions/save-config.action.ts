a6330470d43bdd05ab5e64520118bbc387b5a4ef
JohanAff <112640003+JohanAff@users.noreply.github.com>
2023-12-19 19:40:06 +0100

    feat(etl): add error management for etl (#187)

--- src/etl/actions/save-config.action.ts
+++ src/etl/actions/save-config.action.ts
@@ -1,4 +1,5 @@
 import { Fetcher } from '../../common/api';
+import { getSherlError } from '../../common/utils';
 import { endpoints } from '../api/endpoint';
 import { EtlErr, errorFactory } from '../errors';
 import { IConfigModel, IEtlSaveConfigInputDto } from '../types';
@@ -12,8 +13,15 @@ export const saveConfig = async (
       endpoints.SAVE_CONFIG,
       config,
     );
-    return response.data;
-  } catch (err) {
-    throw errorFactory.create(EtlErr.SAVE_CONFIG_FAILED);
+    switch (response.status) {
+      case 201:
+        return response.data;
+      case 403:
+        throw errorFactory.create(EtlErr.SAVE_CONFIG_FORBIDDEN);
+      default:
+        throw errorFactory.create(EtlErr.SAVE_CONFIG_FAILED);
+    }
+  } catch (error) {
+    throw getSherlError(error, errorFactory.create(EtlErr.SAVE_CONFIG_FAILED));
   }
 };
