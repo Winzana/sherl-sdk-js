9b194af6248495ec6299b154ff71774c8c50dee8
JohanAff <112640003+JohanAff@users.noreply.github.com>
2023-12-19 10:49:46 +0100

    feat(SHER-714): add error virtual money  (#170)

--- src/virtual-money/actions/get-wallet-historical.action.ts
+++ src/virtual-money/actions/get-wallet-historical.action.ts
@@ -1,4 +1,5 @@
 import { Fetcher } from '../../common/api';
+import { getSherlError } from '../../common/utils';
 import { StringUtils } from '../../common/utils/string';
 import { endpoints } from '../api/endpoints';
 import { VirtualMoneyErr, errorFactory } from '../errors';
@@ -16,11 +17,23 @@ export const getWalletHistorical = async (
         historicalId,
       }),
     );
-    if (response.status !== 200) {
-      throw errorFactory.create(VirtualMoneyErr.GET_WALLET_HISTORICAL_FAILED);
+
+    switch (response.status) {
+      case 200:
+        return response.data;
+      case 403:
+        throw errorFactory.create(
+          VirtualMoneyErr.GET_WALLET_HISTORICAL_FAILED_FORBIDDEN,
+        );
+      case 404:
+        throw errorFactory.create(VirtualMoneyErr.WALLET_HISTORICAL_NOT_FOUND);
+      default:
+        throw errorFactory.create(VirtualMoneyErr.GET_WALLET_HISTORICAL_FAILED);
     }
-    return response.data;
   } catch (error) {
-    throw errorFactory.create(VirtualMoneyErr.GET_WALLET_HISTORICAL_FAILED);
+    throw getSherlError(
+      error,
+      errorFactory.create(VirtualMoneyErr.GET_WALLET_HISTORICAL_FAILED),
+    );
   }
 };
