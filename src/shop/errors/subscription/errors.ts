59e9fb89a8494d296ffb3e6156efa33c57dc61ca
jg-winzana <102950530+jg-winzana@users.noreply.github.com>
2023-12-20 14:33:50 +0100

    feat(shop-subscription): update errors code

--- src/shop/errors/subscription/errors.ts
+++ src/shop/errors/subscription/errors.ts
@@ -1,13 +1,22 @@
 import { ErrorFactory } from '../../../common/errors';
 
 export enum SubscriptionErr {
-  FETCH_FAILED = 'subscription/fetch-failed',
+  FIND_ONE_SUBSCRIPTION_WITH_FILTER_FAILED = 'subscription/fetch-failed',
+  FIND_ONE_SUBSCRIPTION_WITH_FILTER_FAILED_FORBIDDEN = 'subscription/fetch-failed-forbidden',
   CANCEL_SUBSCRIPTION_FAILED = 'subscription/cancel-failed',
+  CANCEL_SUBSCRIPTION_FAILED_FORBIDDEN = 'subscription/cancel-failed-forbidden',
+  SUBSCRIPTION_NOT_FOUND = 'subscription/not-found',
 }
 
 export const errors = {
-  [SubscriptionErr.FETCH_FAILED]: 'Failed to fetch subscription',
+  [SubscriptionErr.FIND_ONE_SUBSCRIPTION_WITH_FILTER_FAILED]:
+    'Failed to fetch subscription',
+  [SubscriptionErr.FIND_ONE_SUBSCRIPTION_WITH_FILTER_FAILED_FORBIDDEN]:
+    'Failed to fetch subscription, forbidden',
   [SubscriptionErr.CANCEL_SUBSCRIPTION_FAILED]: 'Failed to cancel subscription',
+  [SubscriptionErr.CANCEL_SUBSCRIPTION_FAILED_FORBIDDEN]:
+    'Failed to cancel subscription, forbidden',
+  [SubscriptionErr.SUBSCRIPTION_NOT_FOUND]: 'Subscription not found',
 };
 
 export const errorFactory = new ErrorFactory<SubscriptionErr>(
