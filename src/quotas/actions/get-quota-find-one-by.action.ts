9ab490c0fe41191dc6b56ac0605882895d31487d
jg-winzana <102950530+jg-winzana@users.noreply.github.com>
2023-12-28 12:26:17 +0100

    docs(quotas): update documentation (#206)

--- src/quotas/actions/get-quota-find-one-by.action.ts
+++ src/quotas/actions/get-quota-find-one-by.action.ts
@@ -5,6 +5,14 @@ import { IQuotaFilter, IQuota } from '../types';
 import { endpoints } from '../api/endpoints';
 import { getSherlError } from '../../common/utils';
 
+/**
+ * Retrieves a single quota based on the provided filters.
+ *
+ * @param {Fetcher} fetcher - The fetcher object used to make API requests.
+ * @param {IQuotaFilter} filters - Optional filters to apply when fetching the quota.
+ * @returns {Promise<IQuota>} - A promise that resolves to the retrieved quota.
+ * @throws {Error} - Throws an error if the API request fails or the quota is forbidden.
+ */
 export const getQuotaFindOneBy = async (
   fetcher: Fetcher,
   filters?: IQuotaFilter,
