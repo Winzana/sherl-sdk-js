349a712a1a8ab55d87716cc074ba66793982f3ea
johan Affre <johan@winzana.com>
2023-12-28 14:04:22 +0100

    feat(SHER-748): add jsdoc iam

--- src/iam/provider.ts
+++ src/iam/provider.ts
@@ -15,7 +15,6 @@ class IamProvider extends AbstractProvider {
   /**
    * Get all IAM profiles based on the provided filters.
    *
-   * @param {Fetcher} fetcher - The Fetcher instance used for making API requests.
    * @param {IIamProfilesFilters} filters - Filters to apply when fetching IAM profiles.
    * @see {@link https://winzana.github.io/sherl-sdk-js/docs/iam#get-all-iam-profiles Sherl SDK documentation} for further information
    */
@@ -24,7 +23,6 @@ class IamProvider extends AbstractProvider {
   /**
    * Get an IAM profile by its unique identifier (ID).
    *
-   * @param {Fetcher} fetcher - The Fetcher instance used for making API requests.
    * @param {string} id - The unique identifier (ID) of the IAM profile to retrieve.
    * @see {@link https://winzana.github.io/sherl-sdk-js/docs/iam#get-iam-profile-by-id Sherl SDK documentation} for further information
    */
@@ -33,7 +31,6 @@ class IamProvider extends AbstractProvider {
   /**
    * Get an IAM role by its unique identifier (ID).
    *
-   * @param {Fetcher} fetcher - The Fetcher instance used for making API requests.
    * @param {string} id - The unique identifier (ID) of the IAM role to retrieve.
    * @see {@link https://winzana.github.io/sherl-sdk-js/docs/iam#get-iam-role-by-id Sherl SDK documentation} for further information
    */
