1620c32a0ca9cca36e2cd55524871001f2a12352
jg-winzana <102950530+jg-winzana@users.noreply.github.com>
2024-01-23 10:10:49 +0100

    fix(config): fixes error management

--- src/config/actions/get-public-config.action.ts
+++ src/config/actions/get-public-config.action.ts
@@ -1,27 +1,28 @@
+import { SherlError } from '../../common';
 import { Fetcher } from '../../common/api';
 import { endpoints } from '../api/endpoints';
 import { ConfigErr, errorFactory } from '../errors';
 import { StringUtils } from '../../common/utils/string';
 import { IPublicConfig } from '../types';
+import { getSherlError } from '../../common/utils/errors';
 
 export const getPublicConfig = async (
   fetcher: Fetcher,
   code: string,
 ): Promise<IPublicConfig> => {
-  const response = await fetcher.get<IPublicConfig>(
-    StringUtils.bindContext(endpoints.GET_PUBLIC_CONFIG, { code }),
-  );
-
-  if (response.status === 404) {
-    throw errorFactory.create(ConfigErr.NOT_FOUND);
+  try {
+    const response = await fetcher.get<IPublicConfig>(
+      StringUtils.bindContext(endpoints.GET_PUBLIC_CONFIG, { code }),
+    );
+    return response.data;
+  } catch (error: SherlError | Error | any) {
+    switch ((error as SherlError).data?.status) {
+      case 403:
+        throw errorFactory.create(ConfigErr.FETCH_FORBIDDEN);
+      case 404:
+        throw errorFactory.create(ConfigErr.NOT_FOUND);
+      default:
+        throw getSherlError(error, errorFactory.create(ConfigErr.FETCH_ERROR));
+    }
   }
-
-  if (response.status !== 200) {
-    throw errorFactory.create(ConfigErr.FETCH_ERROR);
-  }
-
-  return {
-    code,
-    value: response.data,
-  };
 };
--- src/config/errors.ts
+++ src/config/errors.ts
@@ -2,10 +2,16 @@ import { ErrorFactory } from '../common/errors';
 
 export enum ConfigErr {
   FETCH_ERROR = 'config/fetch-failed',
+  FETCH_FORBIDDEN = 'config/fetch-forbidden',
+  SET_CONFIG_ERROR = 'config/set-failed',
+  SET_CONFIG_FORBIDDEN = 'config/set-forbidden',
   NOT_FOUND = 'config/not-found',
 }
 export const errors = {
   [ConfigErr.FETCH_ERROR]: 'Failed to fetch config API',
+  [ConfigErr.FETCH_FORBIDDEN]: 'Failed to fetch config API, forbidden',
+  [ConfigErr.SET_CONFIG_ERROR]: 'Failed to set config',
+  [ConfigErr.SET_CONFIG_FORBIDDEN]: 'Failed to set config, forbidden',
   [ConfigErr.NOT_FOUND]: 'Config not found',
 };
 
