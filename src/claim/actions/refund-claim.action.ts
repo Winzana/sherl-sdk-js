61a9175dc6da0531209081a0fec52cf002fc5f04
jg-winzana <102950530+jg-winzana@users.noreply.github.com>
2024-01-29 11:00:16 +0100

    fix(claim): fixes error management (#221)

--- src/claim/actions/refund-claim.action.ts
+++ src/claim/actions/refund-claim.action.ts
@@ -1,3 +1,4 @@
+import { SherlError } from '../../common';
 import { Fetcher } from '../../common/api';
 import { getSherlError } from '../../common/utils/errors';
 import { StringUtils } from '../../common/utils/string';
@@ -21,17 +22,18 @@ export const refundClaim = async (
       {},
     );
 
-    switch (response.status) {
-      case 200:
-        return response.data;
+    return response.data;
+  } catch (error: SherlError | Error | any) {
+    switch ((error as SherlError).data?.status) {
       case 403:
-        throw errorFactory.create(ClaimErr.REFUND_CLAIM_FORBIDDEN_ERROR);
+        throw errorFactory.create(ClaimErr.REFUND_CLAIM_FORBIDDEN);
       case 404:
         throw errorFactory.create(ClaimErr.CLAIM_NOT_FOUND);
       default:
-        throw errorFactory.create(ClaimErr.REFUND_CLAIM_FAILED);
+        throw getSherlError(
+          error,
+          errorFactory.create(ClaimErr.REFUND_CLAIM_FAILED),
+        );
     }
-  } catch (err) {
-    throw getSherlError(err, errorFactory.create(ClaimErr.REFUND_CLAIM_FAILED));
   }
 };
